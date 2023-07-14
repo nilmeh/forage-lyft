76af9222d2d122845c48f18e89951744b9d7ee95
niloy <niloymeharchandani@gmail.com>
2023-07-14 22:50:44 +0200

    Third Commit

--- batteries/spindler_battery.py
+++ batteries/spindler_battery.py
@@ -7,7 +7,7 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
         
     def battery_should_be_serviced(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 3)
 
         if service_threshold_date < datetime.today().date():
             return True
